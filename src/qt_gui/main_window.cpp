fce95a0f21638a676d24e04f1da8febb1c3d23c8
brad0demx <brad3z.howard@gmail.com>
2025-10-10 22:37:12 +0100

    Linux fix

--- src/qt_gui/main_window.cpp
+++ src/qt_gui/main_window.cpp
@@ -1,4 +1,3 @@
-// SPDX-FileCopyrightText: Copyright 2025 shadPS4 Emulator Project
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include "SDL3/SDL_events.h"
@@ -1279,7 +1278,7 @@ tr("No emulator version was selected.\nThe Version Manager menu will then open.\
 #ifdef Q_OS_WIN
     exeName = "/shadPS4.exe";
 #elif defined(Q_OS_LINUX)
-    exeName = "/shadPS4.AppImage";
+    exeName = "/Shadps4-sdl.AppImage";
 #elif defined(Q_OS_MACOS)
     exeName = "/shadPS4";
 #endif
@@ -1322,7 +1321,7 @@ void MainWindow::RestartEmulator() {
 #ifdef Q_OS_WIN
     exeName = "/shadPS4.exe";
 #elif defined(Q_OS_LINUX)
-    exeName = "/shadPS4.AppImage";
+    exeName = "/Shadps4-sdl.AppImage";
 #elif defined(Q_OS_MACOS)
     exeName = "/shadPS4";
 #endif
